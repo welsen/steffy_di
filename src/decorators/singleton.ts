eaaedf02f1f037644123d49353dea0dd6990fab2
Onofer Dávid <david.onofer@icellmobilsoft.hu>
2021-03-13 09:07:26 +0100

    feat(override)

--- src/decorators/singleton.ts
+++ src/decorators/singleton.ts
@@ -1,6 +1,7 @@
+import Identifier from '../types/identifier';
 import Injectable from './injectable';
 
-function Singleton(identifier = null): ClassDecorator {
+function Singleton(identifier: Identifier<any> | null = null): ClassDecorator {
   return Injectable(identifier);
 }
 
--- src/decorators/transient.ts
+++ src/decorators/transient.ts
@@ -1,7 +1,8 @@
 import Injectable from './injectable';
 import FACTORY_AS from '../enums/factory-as';
+import Identifier from '../types/identifier';
 
-function Transient(identifier = null): ClassDecorator {
+function Transient(identifier: Identifier<any> | null = null): ClassDecorator {
   return Injectable(identifier, FACTORY_AS.TRANSIENT);
 }
 
